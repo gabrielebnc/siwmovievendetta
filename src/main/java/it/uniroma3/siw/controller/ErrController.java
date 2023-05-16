49f6ffbe7da61faedc856987c3bf7383c640912e
Gabriele Bianchi <gabriele.bianchi21@gmail.com>
2023-05-16 11:46:43 +0200

    fixes and /error handling

---
+++ src/main/java/it/uniroma3/siw/controller/ErrController.java
@@ -0,0 +1,9 @@
+package it.uniroma3.siw.controller;
+
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ErrController implements ErrorController {
+
+}
